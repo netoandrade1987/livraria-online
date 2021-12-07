9e8054d4b395470248ae017ff47232ef62d8128d
netoandrade1987 <neto_andrade_21@hotmail.com>
2021-12-07 12:08:01 -0300

    Adicionando /home e alteração na classe SecurityConfiguration

--- src/main/java/br/com/alura/livrariaonline/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/livrariaonline/infra/security/SecurityConfigurations.java
@@ -33,8 +33,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	@Bean
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
+	public  AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 	@Override
@@ -50,6 +50,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 		
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.antMatchers(HttpMethod.GET, "/home").permitAll()
 		.antMatchers("/usuarios/**").hasRole("ADMIN")
 		.anyRequest().authenticated()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
